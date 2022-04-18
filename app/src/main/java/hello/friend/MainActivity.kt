59d8ba74692c72848e35cf014388ee54c617927c
Koerkkoe <korkko.sonja@gmail.com>
2022-04-18 22:32:25 +0300

    Added menu to MainActivity and user can logout

--- app/src/main/java/hello/friend/MainActivity.kt
+++ app/src/main/java/hello/friend/MainActivity.kt
@@ -1,11 +1,45 @@
 package hello.friend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnChat: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        btnChat = findViewById(R.id.btnChat)
+
+        btnChat.setOnClickListener {
+            val intent = Intent(this@MainActivity, ChatActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.logout) {
+            //logout
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this@MainActivity,LoginActivity::class.java))
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
