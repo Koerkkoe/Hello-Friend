1fa19972b2e8e1a036a2d7fb11fda28862319a45
Koerkkoe <korkko.sonja@gmail.com>
2022-04-20 12:06:37 +0300

    Created rounded Select Photo button to SignUpActivity

--- app/src/main/java/hello/friend/SignUpActivity.kt
+++ app/src/main/java/hello/friend/SignUpActivity.kt
@@ -1,15 +1,19 @@
 package hello.friend
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class SignUpActivity : AppCompatActivity () {
 
@@ -17,8 +21,12 @@ class SignUpActivity : AppCompatActivity () {
     private lateinit var editEmail: EditText
     private lateinit var editPassword: EditText
     private lateinit var btnSignUp: Button
+    private lateinit var accountAlready: TextView
+    private lateinit var btnSelectPhoto: Button
     private lateinit var auth: FirebaseAuth
     private lateinit var dbRef: DatabaseReference
+    private lateinit var filepath: Uri
+    private var storageRef: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +40,13 @@ class SignUpActivity : AppCompatActivity () {
         editEmail = findViewById(R.id.editEmail)
         editPassword = findViewById(R.id.editPassword)
         btnSignUp = findViewById(R.id.btnSignUp)
+        btnSelectPhoto = findViewById(R.id.btnSelectPhoto)
+        accountAlready = findViewById(R.id.accountAlready)
+
+        accountAlready.setOnClickListener {
+            val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         btnSignUp.setOnClickListener {
             val name = editName.text.toString()
@@ -40,7 +55,22 @@ class SignUpActivity : AppCompatActivity () {
 
             signUp(name, email, password)
         }
+
+        storageRef = FirebaseStorage.getInstance().reference
+
+        btnSelectPhoto.setOnClickListener {
+            uploadFile()
+        }
     }
+
+        private fun uploadFile() {
+
+            val intent = Intent()
+            intent.setType("images/*")
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent, "Select Photo"), 111)
+
+        }
 
     private fun signUp(name: String, email: String, password: String) {
         // Create a user
@@ -51,7 +81,10 @@ class SignUpActivity : AppCompatActivity () {
                     addUserToDatabase(name,email,auth.currentUser?.uid!!)
                     // Log in a user
                     val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    finish()
                     startActivity(intent)
+
+                    uploadImageToFirebaseStorage()
 
                 } else {
                     Toast.makeText(this@SignUpActivity, "Error", Toast.LENGTH_SHORT).show()
@@ -63,5 +96,21 @@ class SignUpActivity : AppCompatActivity () {
         dbRef = FirebaseDatabase.getInstance("https://hello-friend-app-default-rtdb.europe-west1.firebasedatabase.app/").reference
         // Create a unique id for every user
         dbRef.child("User").child(uid).setValue(User(name,email,uid))
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+
+        //val filename = UUID.randomUUID().toString()
+        val storageRef = FirebaseStorage.getInstance().getReference("gs://hello-friend-app.appspot.com")
+
+        storageRef.putFile(filepath)
+            .addOnSuccessListener {
+                Log.d("SignUpActivity", "Uploaded image: ${it.metadata?.path}")
+            }
+        storageRef.downloadUrl.addOnSuccessListener {
+            Log.d("SignUpActivity", "File location: $it")
+
+        }
+
     }
 }
