462a95017aa92a9c6c811483c44dbd811f9aa8b3
Koerkkoe <korkko.sonja@gmail.com>
2022-04-26 17:46:35 +0300

    Fixed UserAdapter and UserData

--- app/src/main/java/hello/friend/UserAdapter.kt
+++ app/src/main/java/hello/friend/UserAdapter.kt
@@ -8,26 +8,29 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.auth.User
 
-class UserAdapter (private val context: Context, private val userList: ArrayList<User>):
+class UserAdapter(private val context: Context, private val userList: ArrayList<UserData>):
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.activity_user, parent, false)
-        return UserViewHolder(view)
+        val itemView = LayoutInflater.from(context).inflate(R.layout.activity_user,
+        parent, false)
+        return UserViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        val currentUser = userList[position]
 
-        //holder.textName.text = currentUser.name
+        val currentUser = userList[position]
+        holder.textName.text = currentUser.name
+
     }
 
     override fun getItemCount(): Int {
+        //tells the adapter how many items there are in the recyclerview
         return userList.size
     }
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textName = itemView.findViewById<TextView>(R.id.txt_name)
+        val textName: TextView = itemView.findViewById(R.id.txtName)
+
     }
 }
--- app/src/main/java/hello/friend/UserData.kt
+++ app/src/main/java/hello/friend/UserData.kt
@@ -1,17 +1,18 @@
 package hello.friend
 
-
-class User {
+class UserData {
     var name: String? = null
     var email: String? = null
     var uid: String? = null
+    var imageUrl: String? = null
 
     constructor() {}
 
-    constructor(name: String?, email: String?, uid: String?) {
+    constructor(name: String?, email: String?, uid: String?, imageUrl: String?) {
         this.name = name
         this.email = email
         this.uid = uid
+        this.imageUrl = imageUrl
 
     }
 }
